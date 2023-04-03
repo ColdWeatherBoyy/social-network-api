882a5f8ac65d44a0c86c2450cb25e88ffe8d67c1
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-04-03 15:50:53 -0400

    sets up models and index.js for models

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,1 +1,34 @@
 const { Schema, Types } = require("mongoose");
+
+const reactionSchema = new Schema(
+	{
+		reactionId: {
+			type: Schema.Types.ObjectId,
+			default: () => new Types.ObjectId(),
+		},
+		reactionBody: {
+			type: String,
+			require: true,
+			maxLength: 280,
+		},
+		username: {
+			type: String,
+			require: true,
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+		},
+	},
+	{
+		toJSON: { getters: true },
+		id: false,
+	}
+);
+
+reactionSchema.virtual("formatDate").get(function () {
+	this.createdAt = this.createdAt.toLocaleString();
+	return this.createdAt;
+});
+
+module.exports = reactionSchema;
