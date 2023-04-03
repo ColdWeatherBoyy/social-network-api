3cec2c25729a5d903c9395d596c0b13e8b7e033f
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-04-03 17:51:15 -0400

    adds friends routes

--- controller/userController.js
+++ controller/userController.js
@@ -53,4 +53,42 @@ module.exports = {
 			res.status(500).send({ message: err });
 		}
 	},
+	async createFriend(req, res) {
+		try {
+			const friend = await User.findOne({ _id: req.params.friendId });
+			const user = await User.findOne({ _id: req.params.userId });
+			if (!friend) {
+				res.status(404).send({ message: "Friend ID not found" });
+			} else if (!user) {
+				res.status(404).send({ message: "User ID not found" });
+			} else if (user.friends.includes(friend)) {
+				res.status(400).send({ message: "Friend already added" });
+			}
+
+			user.friends.push(friend);
+			await user.save();
+
+			res.status(200).json(friend);
+		} catch (err) {
+			res.status(500).send({ message: err });
+		}
+	},
+	async deleteFriend(req, res) {
+		try {
+			const user = await User.findOne({ _id: req.params.userId });
+
+			if (!user) {
+				res.status(404).send({ message: "User ID not found" });
+			} else if (!user.friends.includes(req.params.friendId)) {
+				res.status(404).send({ message: "Friend ID not found" });
+			}
+
+			user.friends.pull(req.params.friendId);
+			await user.save();
+
+			res.status(200).send({ message: "Friend removed successfully" });
+		} catch (err) {
+			res.status(500).send({ message: err });
+		}
+	},
 };
