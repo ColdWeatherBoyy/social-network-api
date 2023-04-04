39c00fe925cd521e7b677346772c37ab3e0204e7
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-04-04 15:45:40 -0400

    adds thought controller

---
+++ controller/thoughtController.js
@@ -0,0 +1,87 @@
+const { User, Thought } = require("../models/index");
+const { reactionSchema } = require("../models/Reaction");
+
+module.exports = {
+	// get all thoughts
+	async getThoughts(req, res) {
+		try {
+			const thoughts = await Thought.find({});
+			res.status(200).json(thoughts);
+		} catch (err) {
+			res.status(500).send({ message: err });
+		}
+	},
+	// get a single thought by _id
+	async getSingleThought(req, res) {
+		try {
+			const thought = await Thought.find({ _id: req.params.thoughtId });
+			res.status(200).json(thought);
+		} catch (err) {
+			res.status(500).send({ message: err });
+		}
+	},
+	// create a new thought (push created thought to associated uer's thoughts array)
+	async createThought(req, res) {
+		try {
+			const thought = await Thought.create(req.body);
+
+			const user = await User.findOneAndUpdate(
+				{ _id: req.body.userId },
+				{ $addToSet: { thoughts: thought._id } },
+				{ new: true }
+			);
+
+			// check if user found
+			if (!user) {
+				return res
+					.status(404)
+					.send({ message: "User not found, but application created" });
+			}
+
+			res.status(202).json(`${thought} created and added to ${user}`);
+		} catch (err) {
+			res.status(500).send({ message: err });
+		}
+	},
+	// update a thought by id
+	async updateThought(req, res) {
+		try {
+			const thought = await Thought.findOne(
+				{ _id: req.params.thoughtId },
+				{ $set: req.body },
+				{ runValidators: true, new: true }
+			);
+
+			if (!thought) {
+				return res.status(404).json({ message: "No thought with that ID found" });
+			}
+		} catch (err) {
+			res.status(500).send({ message: err });
+		}
+	},
+	// delete a thought by id (remove from user array also)
+	async deleteThought(req, res) {
+		try {
+			const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
+
+			if (!thought) {
+				return res.status(404).json({ message: "No thought with that ID found" });
+			}
+
+			const user = await User.findOneAndUpdate(
+				{ thoughts: req.params.thoughtId },
+				{ pull: { thoughts: req.params.thoughtId } },
+				{ new: true }
+			);
+
+			if (!user) {
+				return res
+					.status(404)
+					.json({ message: "Thought deleted, but no user with that ID found)" });
+			}
+			res.status(200).json({ message: "Thought deleted and removed from User document" });
+		} catch (err) {
+			res.status(500).send({ message: err });
+		}
+	},
+};
