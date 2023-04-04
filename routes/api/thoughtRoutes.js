9d08c91d710dd0264df9a8c22b3e216bf2230e72
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-04-04 15:47:38 -0400

    adds routes for thought

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -5,4 +5,15 @@ const {
 	getSingleThought,
 	createThought,
 	deleteThought,
+	updateThought,
 } = require("../../controller/thoughtController");
+
+// /api/thoughts
+router.route("/").get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+	.route("/:thoughtId")
+	.get(getSingleThought)
+	.delete(deleteThought)
+	.put(updateThought);
